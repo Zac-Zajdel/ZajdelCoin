d1e4f19b7dc295c84466d46d34271bf2870215d2
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2020-04-16 19:46:56 -0400

    Implemented verification of signature inside wallet class and created testing to validate logic.

--- backend/Wallet/wallet.py
+++ backend/Wallet/wallet.py
@@ -5,6 +5,7 @@ from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 
 class Wallet:
   """
@@ -29,14 +30,38 @@ class Wallet:
       ec.ECDSA(hashes.SHA256())
     )
 
+  @staticmethod
+  def verify(public_key, data, signature):
+    """
+    Verify a signature based on the original public key and data.
+    The public key created from the private key has the built-in verify() method.
+    This method returns true if the private key associated with the public key was used to sign the transaction confirming the signature
+    and the data has not been tampered with between signing and verification.
+    """
+    try:
+      public_key.verify(
+        signature,
+        json.dumps(data).encode('utf-8'),
+        ec.ECDSA(hashes.SHA256())
+      )
+      return True
+    except InvalidSignature:
+      return False
+
 def main():
   wallet = Wallet()
   print(f'Wallet: {wallet.__dict__}')
 
   data = { 'foo': 'bar'}
   signature = wallet.sign(data)
-
   print(f'Signature: {signature}')
+
+  should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+  print(f'should_be_valid: {should_be_valid}')
+
+  # Should fail as we are creating a different wallet instance and 
+  should_be_invalid = Wallet.verify(Wallet().public_key, data, signature)
+  print(f'should_be_invalid: {should_be_invalid}')
 
 if __name__ == "__main__":
     main()
